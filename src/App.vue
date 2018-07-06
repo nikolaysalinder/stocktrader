887258a37cfbadd57e9db72c8538e7452fee04b6
nikolaysalinder <nikolay.salinder@gmail.com>
2018-07-06 13:53:39 +0300

    prepared filese

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App',
+  vasya: '',
 };
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+</script>
+<style lang="sass">
+div
+  display: block;
+  display: vasya
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {};
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+h1
+  display: block
+</style>
